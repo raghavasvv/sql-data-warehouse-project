a22057b7cd44dc79c12a8a1fd249738e97cc2c36
raghavasvv <47350420+raghavasvv@users.noreply.github.com>
2026-02-19 19:41:14 -0600

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,21 @@
-
+============================================
+CREATING DATABASE AND SCHEMAS
+============================================
+  This script creates new database named DWH 
+  and additionally it also create 3 schemas: 
+  bronze, 
+  silver, 
+  gold.
+=============================================
+-- Creating the Database DWH --
+CREATE DATABASE DWH;
+GO
+USE DWH;
+GO
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
